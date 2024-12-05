b9e00d8c246d50d033ef24aaad03a7fc80a84af3
sebousan <sebousan@gmail.com>
2024-12-05 15:58:55 +0100

    js

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Utils
+import './utils/global';
+
+// Vendors
+import './vendors/bootstrap';
+
+// Components
+import './components/carousel';
+import './components/gallery';
+import './components/map';
+import './components/menu';
+import './components/toc';
